f03510c5f37600abd952fe5e105f913cdb6145f7
Jass Khaira <jaspreetsinghkhaira@gmail.com>
2020-08-24 15:07:22 -0400

    --

--- Code/Basics/managers.py
+++ Code/Basics/managers.py
@@ -45,3 +45,54 @@ class CaptureManager(object):
             return self._videoFilename is not None
 
 
+        def enterFrame(self):
+            """Capture the next frame, if any. """
+            # But first, check that any previous frame was exited.
+            assert not self._enteredFrame, \
+            'previous enterFrame() had no matching exitFrame()'
+            if self._capture is not None:
+                self._enteredFrame = self._capture.grab()
+
+
+
+
+
+        def exitFrame(self):
+            """Draw to window. Write to files. Release the frame"""
+
+            # Check whether any grabbed frame is retrievable.
+            # The getter may retrive and cache the frame.
+
+            if self._frame in None:
+                self._enteredFrame = False
+                return
+
+            # Update the FPS estimate and related variable.
+            if self._frameElapsed == 0
+                self._startTime = time.time()
+
+            else:
+                timeElapsed = time.time() - self._startTime
+                self._fpsEstimate = self._frameElapsed / timeElapsed
+            self._framesElapsed += 1
+            # Draw to the window, if any.
+            if self.previewWindowManager is not None:
+                if self.shouldMirrorPreview:
+                    mirroredFrame = np.fliplr(self._frame)
+            self.previewWindowManager.show(mirroredFrame)
+
+            else:
+                self.previewWindowManager.show(self._frame)
+            # Write to the image file, if any.
+            if self.isWritingImage:
+                cv2.imwrite(self._imageFilename, self._frame)
+            self._imageFilename = None
+
+            # Write to the video file, if any.
+            self._writeVideoFrame()
+            # Release the frame.
+            self._frame = None
+            self._enteredFrame = False
+
+
+
